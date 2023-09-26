2f5effb3ee4bca6110b366a2937a78c17b92f5b3
Madhusha <madhushahansini.perera@gmail.com>
2023-09-26 10:58:59 +0530

    1

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -9,4 +9,16 @@ Class Admin extends Controller{
         $this->view('admin/dashboard');
     }
 
+    public function profile($method = null){
+
+        $user = new User();
+
+        $data['user'] = $row = $user->first(['user_id' => Auth::getUser_id()]);
+
+        if(empty($method)) $this->view('common/profile/overview', $data);
+
+
+
+    }
+
 }
