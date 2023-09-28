466fd6c08d19f989e84460dff23a4263b8d43886
NightHawkNB <nipunbathiya1256@outlook.com>
2023-09-28 11:10:22 +0530

    Event Details changes

---
+++ app/views/common/events/card.view.php
@@ -0,0 +1,20 @@
+<a href="#Charlotte" class="card-profile">
+    <div class="dis-flex-col pad-10 al-it-ce bg-black-1 bor-rad-5">
+        <img src="<?= ROOT ?>/assets/images/users/user_01.jpg" alt="user_01" class="profile-image-2">
+        <h3 class="txt-c-white f-mooli text-small">Charlotte Brown</h3>
+    </div>
+</a>
+
+<a href="#John" class="card-profile">
+    <div class="dis-flex-col pad-10 al-it-ce bg-black-1 bor-rad-5">
+        <img src="<?= ROOT ?>/assets/images/users/user_02.jpg" alt="user_02" class="profile-image-2">
+        <h3 class="txt-c-white f-mooli text-small">Charlotte Brown</h3>
+    </div>
+</a>
+
+<a href="#Charlotte" class="card-profile">
+    <div class="dis-flex-col pad-10 al-it-ce bg-black-1 bor-rad-5">
+        <img src="<?= ROOT ?>/assets/images/users/user_01.jpg" alt="user_01" class="profile-image-2">
+        <h3 class="txt-c-white f-mooli text-small">Charlotte Brown</h3>
+    </div>
+</a>
