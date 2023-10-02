d3024e3da2b9af3365ca2d972c19fc2778b07b5a
AkilaDemotte <akilamilindademotte@gmail.com>
2023-10-02 14:56:28 +0530

    list_complain view(update crud)

--- app/views/pages/complains/list_complain.view.php
+++ app/views/pages/complains/list_complain.view.php
@@ -3,10 +3,17 @@
 <body>
 <div class="main-wrapper">
     <?php $this->view('includes/header') ?>
-
     <main class="bg-lightgray dis-flex al-it-ce ju-co-ce">
         <div class="bg-black-2 flex-1 pad-20">
-          <h1>comlain list</h1>
+<!--          <h1 class="txt-c-white">comlain list</h1>-->
+            <div class="txt-c-white">
+                <?php
+                foreach ($complains as $complain) {
+//                    show($complain);
+                    $this->view('pages/complains/single', (array)$complain);
+                }
+                ?>
+            </div>
         </div>
     </main>
 
