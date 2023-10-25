4106f8a08cdaa7372b645d7a7bf8f809b55ced1f
NightHawkNB <nipunbathiya1256@outlook.com>
2023-10-26 04:53:04 +0530

    View event from db

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -6,32 +6,35 @@ class Home extends Controller{
     }
 
     public function events($id = null, $method = null) {
-        $data['record'] = array(
-            array(
-                'event_id' => 1,
-                'name' => 'Yaathra',
-                'details' => "Details About the Event",
-                'ticketing_plan' => "5000*20/3000*30/2000*50",
-                'venue_id' => 2,
-                'band_id' => 1,
-                'vuser_id' => 2,
-                'venueO_id' => 3,
-                'DateTime' => "2023-11-13",
-                'image' => "event-01.jpeg"
-            ),
-            array(
-                'event_id' => 2,
-                'name' => 'Melody Friday',
-                'details' => "Details About the Second Event",
-                'ticketing_plan' => "4500*10/2500*30/1500*50",
-                'venue_id' => 4,
-                'band_id' => 3,
-                'vuser_id' => 1,
-                'venueO_id' => 4,
-                'DateTime' => "2023-10-20",
-                'image' => "event-02.jpeg"
-            )
-        );
+//        $data['record'] = array(
+//            array(
+//                'event_id' => 1,
+//                'name' => 'Yaathra',
+//                'details' => "Details About the Event",
+//                'ticketing_plan' => "5000*20/3000*30/2000*50",
+//                'venue_id' => 2,
+//                'band_id' => 1,
+//                'vuser_id' => 2,
+//                'venueO_id' => 3,
+//                'DateTime' => "2023-11-13",
+//                'image' => "event-01.jpeg"
+//            ),
+//            array(
+//                'event_id' => 2,
+//                'name' => 'Melody Friday',
+//                'details' => "Details About the Second Event",
+//                'ticketing_plan' => "4500*10/2500*30/1500*50",
+//                'venue_id' => 4,
+//                'band_id' => 3,
+//                'vuser_id' => 1,
+//                'venueO_id' => 4,
+//                'DateTime' => "2023-10-20",
+//                'image' => "event-02.jpeg"
+//            )
+//        );
+
+        $event = new Event();
+        $data['record'] = $event->query("SELECT * FROM event");
 
         if(empty($id)) $this->view('events', $data);
         else if($method == "buy") {
