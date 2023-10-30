91ede61f32083d667bb8c164a377af9f06b45d45
NightHawkNB <nipunbathiya1256@outlook.com>
2023-10-30 13:27:02 +0530

    UI Optimizations

--- app/views/common/ads/create-ad.view.php
+++ app/views/common/ads/create-ad.view.php
@@ -4,31 +4,37 @@
 <div class="main-wrapper">
     <?php $this->view('includes/header') ?>
 
+    <script src="<?= ROOT ?>/assets/scripts/validation.js" defer></script>
+
     <main class="dashboard-main">
         <section class="cols-2 sidebar">
             <?php $this->view('includes/sidebar') ?>
         </section>
-        <section class="dis-flex-col al-it-ce pad-20 gap-10 cols-10">
+        <section class="dis-flex-col al-it-ce pad-20 gap-10">
 
-            <form method="post" class="bg-black-1 pad-20 bor-rad-5 dis-flex-col">
-                <div class="profile-input <?= (!empty($errors['title'])) ? 'error' : '' ?>">
+            <form method="post" id="ad_form" class="bg-lightgray txt-c-black al-it-ce pad-20 bor-rad-5 dis-flex-col flex-1 ju-co-ce">
+                <div class="profile-input-2 <?= (!empty($errors['title'])) ? 'error' : '' ?>">
                     <label for="title">Title</label>
-                    <input type="text" name="title" value="<?php if(!empty($errors['title'])) echo $errors->title ?>">
+                    <input type="text" id="title" name="title" value="<?php if(!empty($errors['title'])) echo $errors->title ?>">
+                    <div class="error"></div>
                 </div>
 
-                <div class="profile-input <?= (!empty($errors['details'])) ? 'error' : '' ?>">
+                <div class="profile-input-2 <?= (!empty($errors['details'])) ? 'error' : '' ?>">
                     <label for="details">Details</label>
-                    <textarea name="details"><?php if(!empty($errors['details'])) echo $errors->details ?></textarea>
+                    <textarea name="details" id="details" class="bor-rad-5 pad-10"><?php if(!empty($errors['details'])) echo $errors->details ?></textarea>
+                    <div class="error"></div>
                 </div>
 
-                <div class="profile-input <?= (!empty($errors['rates'])) ? 'error' : '' ?>">
+                <div class="profile-input-2 <?= (!empty($errors['rates'])) ? 'error' : '' ?>">
                     <label for="rates">Rates</label>
-                    <input type="text" name="rates" value="<?php if(!empty($errors['rates'])) echo $errors->rates ?>">
+                    <input type="text" id="rates" name="rates" value="<?php if(!empty($errors['rates'])) echo $errors->rates ?>">
+                    <div class="error"></div>
                 </div>
 
-                <div class="profile-input <?= (!empty($errors['image'])) ? 'error' : '' ?>">
+                <div class="profile-input-2 <?= (!empty($errors['image'])) ? 'error' : '' ?>">
                     <label for="image">Image</label>
-                    <input type="text" name="image" value="<?php if(!empty($errors['image'])) echo $errors->image ?>">
+                    <input type="text" id="image" name="image" value="<?php if(!empty($errors['image'])) echo $errors->image ?>">
+                    <div class="error"></div>
                 </div>
 
                 <div class="wid-100 dis-flex ju-co-ce">
--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,3 +1,6 @@
+import "validation"
+
+
 const daysTag = document.querySelector(".days"),
 currentDate = document.querySelector(".current-date"),
 prevNextIcon = document.querySelectorAll(".icons span");
