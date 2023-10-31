8c65b5689d228ef919e1781e0b27c55d95ccb664
NightHawkNB <nipunbathiya1256@outlook.com>
2023-10-31 12:43:14 +0530

    Support

--- app/views/pages/complaints/create_complaint.view.php
+++ app/views/pages/complaints/create_complaint.view.php
@@ -4,14 +4,20 @@
 <div class="main-wrapper">
     <?php $this->view('includes/header') ?>
 
-    <main class="bg-lightgray dis-flex al-it-ce ju-co-ce">
-        <div class="bg-black-2 flex-1 pad-20">
-          <form method="POST">
-            <label for="details">Type your complain:</label>
+    <main class="bg-lightgray dis-flex al-it-ce ju-co-ce wid-100">
+        <div class="bg-lightgray txt-c-black flex-1 pad-20 dis-flex ju-co-ce">
+          <form method="POST" id="complaint-form" class="dis-flex-col ju-co-ce wid-50">
+            <label for="details" class="txt-c-black">Type your complaint :</label>
             <textarea name="details" id="details" cols="30" rows="10"></textarea>
-            <label for="files">Add Files:</label>
-            <input type="text" name="files"  >
-            <button class="btn-lay-2 btn-anima-hover bg-indigo-2 txt-c-white" type="submit">Submit</button>
+            <div class="error"></div>
+            <label for="files" class="txt-c-black">Add Files :</label>
+            <input type="text" name="files" class="input">
+            <div class="error"></div>
+
+          <div class="wid-100 dis-flex ju-co-ce">
+              <button class="btn-lay-2 btn-anima-hover bg-indigo-2 txt-c-white" type="submit">Submit</button>
+          </div>
+
           </form>
         </div>
     </main>
--- app/views/pages/support/overview.view.php
+++ app/views/pages/support/overview.view.php
@@ -4,25 +4,31 @@
 <div class="main-wrapper">
     <?php $this->view('includes/header') ?>
 
-    <main class="bg-lightgray dis-flex al-it-ce ju-co-ce">
-
-        <div class="bg-white dis-flex-col gap-20 flex-1 pad-20">
-            <h1 class="txt-c-black">complaint list</h1>
-            <a class="push-right" href="<?= ROOT ?>/home/complaint">
-                <button class="btn-lay-2 btn-anima-hover hover-pointer">Create complaint</button>
-            </a>
-            <div class="txt-c-white dis-flex-col gap-10">
-                <?php
-                if(!empty($complaints)) {
-                    foreach ($complaints as $complaint) {
-                        $this->view('pages/complaints/single', (array)$complaint);
-                    }
-                } else {
-                    echo "No complaints to show";
-                }
-                ?>
+    <main class="dashboard-main">
+        <section class="cols-2 sidebar">
+            <?php $this->view('includes/sidebar') ?>
+        </section>
+        <section class="cols-10 pad-20 mar-bot-10">
+            <div class="bg-white dis-flex-col gap-20 bg-lightgray flex-1 pad-20">
+                <div class="dis-flex ju-co-sb">
+                    <h1 class="txt-c-black">Your Complaints</h1>
+                    <a class="push-right" href="<?= ROOT ?>/home/complaint">
+                        <button class="btn-lay-2 btn-anima-hover hover-pointer">Create complaint</button>
+                    </a>
+                </div>
+                <div class="dis-flex-col bg-trans gap-10 pad-20 txt-ali-cen">
+                    <?php
+                        if(!empty($complaints)) {
+                            foreach ($complaints as $complaint) {
+                                $this->view('pages/complaints/single', (array)$complaint);
+                            }
+                        } else {
+                            echo "No complaints to show";
+                        }
+                    ?>
+                </div>
             </div>
-        </div>
+        </section>
     </main>
 
     <?php $this->view('includes/footer') ?>
