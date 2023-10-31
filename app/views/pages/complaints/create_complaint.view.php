54b25a49a0eaa9fd4e19c0168bc230668ec3a420
AkilaDemotte <akilamilindademotte@gmail.com>
2023-10-31 12:42:13 +0530

    some new changes

--- app/views/pages/complaints/create_complaint.view.php
+++ app/views/pages/complaints/create_complaint.view.php
@@ -5,7 +5,7 @@
     <?php $this->view('includes/header') ?>
 
     <main class="bg-lightgray dis-flex al-it-ce ju-co-ce">
-        <div class="bg-black-2 flex-1 pad-20">
+        <div class="bg-black-2 flex-1 pad-20 wid-auto">
           <form method="POST">
             <label for="details">Type your complain:</label>
             <textarea name="details" id="details" cols="30" rows="10"></textarea>
