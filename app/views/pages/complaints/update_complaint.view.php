6a09e581896bf6bd92d7348e52e13aa42b317f5e
AkilaDemotte <akilamilindademotte@gmail.com>
2023-10-31 15:05:37 +0530

    There is a error when comment the "Add files section"

--- app/views/pages/complaints/update_complaint.view.php
+++ app/views/pages/complaints/update_complaint.view.php
@@ -4,15 +4,17 @@
 <div class="main-wrapper">
     <?php $this->view('includes/header') ?>
 
-    <main class="bg-lightgray dis-flex al-it-ce ju-co-ce">
-    <div class="bg-black-2 flex-1 pad-20">
-        <?= show($_POST) ?>
+    <main class="bg-lightgray dis-flex al-it-ce ju-co-ce wid-100">
+<!--    <div class="bg-lightgray txt-c-black flex-1 pad-20 dis-flex ju-co-ce">-->
+<!--        --><?php //= show($_POST) ?>
           <form method="POST">
-            <label for="details">Type your complain:</label>
+            <label for="details" class="txt-c-black">Update your complaint:</label>
             <textarea name="details" id="details" cols="30" rows="10"><?= $row->details ?></textarea>
-            <label for="files">Add Files:</label>
-            <input type="text" name="files" value=<?= $row->files ?> >
-            <button class="btn-lay-s btn-anima-hover bg-indigo-2 txt-c-white" type="submit">Submit</button>
+<!--            <label for="files">Add Files:</label>-->
+<!--            <input type="text" name="files" value=--><?php //= $row->files ?><!-- >-->
+              <div class="wid-100 dis-flex ju-co-ce">
+                  <button class="btn-lay-2 btn-anima-hover bg-indigo-2 txt-c-white" type="submit">Submit</button>
+              </div>
           </form>
         </div>
     </main>
