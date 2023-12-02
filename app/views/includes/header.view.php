78ee3c427e0b65d7222adcaef146be0bbb6881c0
NightHawkNB <nipunbathiya1256@outlook.com>
2023-12-02 14:40:14 +0530

    Navigation bar changes

--- app/views/includes/header.view.php
+++ app/views/includes/header.view.php
@@ -54,15 +54,16 @@
         <?php endif; ?>
     </nav>
     <div class="dis-flex gap-20 al-it-ce txt-c-white">
-        <?php if(Auth::logged_in()): ?>
-            <div class="">Hi, <?= ucfirst(Auth::getFname())." ".substr(ucfirst(Auth::getLname()), 0, 1) ?></div>
-        <?php endif; ?>
         <?php if(!Auth::logged_in()): ?>
             <a href="<?= ROOT ?>/login" id="login" class="btn-lay">Login</a>
             <a href="<?= ROOT ?>/signup" id="signup" class="btn-lay">Signup</a>
         <?php else: ?>
             <div class="">
                 <svg class="fill-white" xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg>
+            </div>
+            <div class="dis-flex gap-10 al-it-ce bg-primary bor-rad-5" style="padding: 2px 5px">
+                <img src="<?= $_SESSION['USER_DATA']->image ?>" alt="profile-image" style="width: 35px; border-radius: 50%">
+                Hi, <?= ucfirst(Auth::getFname())." ".substr(ucfirst(Auth::getLname()), 0, 1) ?>
             </div>
             <a href="<?= ROOT ?>/logout" id="logout" class="btn-lay">Logout</a>
         <?php endif; ?>
