beca043f4fd365e7eba3bd0b60c78e9678a8f0ce
AkilaDemotte <akilamilindademotte@gmail.com>
2023-12-11 20:50:56 +0530

    try change some user interfaces in client reservation

--- app/views/client/components/reservation.view.php
+++ app/views/client/components/reservation.view.php
@@ -1,44 +1,21 @@
+<div class="reservations">
 
-<div class="dis-flex wid-100">
-    <div class="bg-white txt-c-black pad-10-20 bor-rad-5 wid-100 dis-flex al-it-ce ju-co-sb ads sh f-poppins" style="gap: 100px">
-        <div class="txt-c-black dis-flex-col gap-10 flex-1">
-            <h4>Name:</h4>
-            <p><?= $fname." ".$lname ?></p>
-        </div>
+    <?php
+    $now = new DateTime();
+    $future_date = new DateTime($start_time);
 
-        <div class="dis-flex-col txt-c-black gap-10">
-            <p class="txt-w-bold">Created date</p>
-            <p><?= $createdDate ?></p>
-        </div>
+    $interval = $future_date->diff($now);
+    ?>
 
-        <div class="dis-flex-col txt-c-black gap-10">
-            <p class="txt-w-bold">Event</p>
-            <p><?= $details ?></p>
-        </div>
+    <p><?= $fname." ".$lname ?></p>
+    <p><?= $start_time ?></p>
+    <p><?= $location ?></p>
+    <p><?= $interval->format("%a days : %h hours : %i minutes") ?></p>
+    <?php
+    if($status == 'pending'){
+        echo "<div style='background-color: lightgreen; border-radius: 10px'> <p><?= {$status} ?></p></div>";
+    }
+    ?>
 
-        <div class="dis-flex-col txt-c-black gap-10">
-            <p class="txt-w-bold">Start date and time</p>
-            <p><?= $start_time ?></p>
-            <p class="txt-w-bold">End date and time</p>
-            <p><?= $end_time ?></p>
-        </div>
 
-        <div class="dis-flex-col txt-c-black gap-10">
-            <p class="txt-w-bold">Location</p>
-            <p><?= $location ?></p>
-        </div>
-
-        <?php
-        $color = "black";
-        if($status == 'Pending') $color = "orange";
-        else if ($status == 'Accepted') $color = "lightgreen";
-        else if ($status == 'Decline') $color = "red";
-        ?>
-
-        <div class="dis-flex-col txt-c-black gap-10">
-            <p class="txt-w-bold">Status</p>
-            <p style="color: <?= $color ?>;"><?= $status ?></p>
-        </div>
-
-    </div>
 </div>
--- app/views/client/reservations.view.php
+++ app/views/client/reservations.view.php
@@ -8,25 +8,51 @@
         <section class="cols-2 sidebar">
             <?php $this->view('includes/sidebar') ?>
         </section>
-        <section class="cols-10 pad-20 mar-bot-10">
-            <div class="dis-flex-col gap-10 bg-grey pad-10 bor-rad-5 txt-c-white" style="height: 100%">
-                <h2 class="mar-0" >Reservation Details</h2>
+<!--        <section class="cols-10 pad-20 mar-bot-10">-->
+<!--            <div class="dis-flex-col gap-10 bg-grey pad-10 bor-rad-5 txt-c-white" style="height: 100%">-->
+<!--                <h2 class="mar-0" >Reservation Details</h2>-->
+<!---->
+<!--                <div class="dis-flex-col gap-10 bg-grey pad-10 bor-rad-5 txt-c-white" style="height: auto">-->
+<!--                    --><?php
+////                    show($data);
+////                    show($reservations);
+//////                    show($reservations[0]);
+//                    foreach ($reservations as $reservation)
+//                    {
+//                        $this->view('client/components/reservation', (array)$reservation);
+//                    }
+//
+//                    ?>
+<!---->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+<!--        </section>-->
+        <section class="wid-100 pad-10 dis-flex-col al-it-ce">
 
-                <div class="dis-flex-col gap-10 bg-grey pad-10 bor-rad-5 txt-c-white" style="height: auto">
-                    <?php
-//                    show($data);
-//                    show($reservations);
-////                    show($reservations[0]);
-                    foreach ($reservations as $reservation)
-                    {
-                        $this->view('client/components/reservation', (array)$reservation);
-                    }
+            <h1 class="mar-10-0 txt-c-white txt-w-bold" style="font-size: 1.5rem">Reservations</h1>
 
-                    ?>
-
+            <div class="component-list wid-80">
+                <div class="reservations">
+                    <p>Service provider's Name</p>
+                    <p>Created date and Time</p>
+                    <p>Location</p>
+                    <p>Time Remaining</p>
+                    <p>Status</p>
+<!--                    <p>Actions</p>-->
                 </div>
 
+                <?php
+                if(!empty($reservations)) {
+                    foreach($reservations as $reservation) {
+                        $this->view('client/components/reservation', (array)$reservation);
+                    }
+                } else {
+                    echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No reservations to show</h3>";
+                }
+                ?>
             </div>
+
         </section>
     </main>
 </div>
