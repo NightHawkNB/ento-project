abccd45be5d23f68347effed7aa6dd9775b7b975
hansimadhusha <madhushahansini.perera@gmail.com>
2023-12-14 17:43:06 +0530

    view ad verifications

---
+++ app/views/admin/adverification.view.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<?php $this->view('includes/head') ?>
+<body>
+<div class="main-wrapper">
+    <?php $this->view('includes/header') ?>
+
+    <main class="dashboard-main">
+        <section class="cols-2 sidebar">
+            <?php $this->view('includes/sidebar') ?>
+        </section>
+
+        <section class="cols-10 dis-flex">
+            <div class="bg-black-2 mar-10 wid-100 dis-flex-col pad-20 gap-10 bor-rad-5" style="justify-content:stretch; align-items:stretch">
+                <a href="profile/verify" class="push-right">
+                    <button class="btn-lay-2 hover-pointer"  style="background-color:purple; text-align:right; border: none" >Filter by</button>
+                </a>
+
+                <div class="flex-1 dis-flex-col gap-10 mar-bot-10 mar-top-10">
+
+                    <?php foreach($ads as $ad){
+                        $this->view('admin/components/singleadverification',(array)$ad);
+                    }
+                    ?>
+
+                </div>
+
+                <div class="dis-flex ju-co-se">
+                    <a href="profile/verify">
+                        <button class="btn-lay-2 push-right hover-pointer"  style="background-color:purple; text-align:right; border: none" >Handle Requests</button>
+                    </a>
+
+                    <a href="profile/verify">
+                        <button class="btn-lay-2 push-right hover-pointer"  style="background-color:purple; text-align:right; border: none" >Pending Requests</button>
+                    </a>
+                </div>
+
+            </div >
+        </section>
+    </main>
+</div>
+</body>
+</html>
--- app/views/includes/sidebar/main/admin.view.php
+++ app/views/includes/sidebar/main/admin.view.php
@@ -6,7 +6,7 @@
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="<?= ROOT ?>/<?= strtolower($_SESSION['USER_DATA']->user_type) ?>/usermng">
+    <a class="nav-link" href="<?= ROOT ?>/<?= strtolower($_SESSION['USER_DATA']->user_type) ?>/adverify">
         <svg class="feather feather-tag" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"/><line x1="7" x2="7.01" y1="7" y2="7"/></svg>
         <span class="link-name">Ad verification</span>
     </a>
