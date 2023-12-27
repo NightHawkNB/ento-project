b879706856f8cf7cef11b090d6d1afa7d14f07db
NightHawkNB <nipunbathiya1256@outlook.com>
2023-12-27 21:34:46 +0530

    login view changes

--- app/views/includes/login.view.php
+++ app/views/includes/login.view.php
@@ -1,7 +1,7 @@
 <html lang="en">
-<?php $this->view('includes/head') ?>
+<?php $this->view('includes/head', ['style' => 'auth/login.css']) ?>
 <body>
-    <div class="main-wrapper auth-page">
+    <div class="dis-flex ju-co-ce al-it-ce pad-20 wid-100 hei-100">
 
         <?php if(message()): ?>
             <div class="alert-msg cols-12"><?= message('', true); ?></div>
@@ -9,27 +9,30 @@
             <div></div>
         <?php endif; ?>
 
-        <div class="dis-flex-col al-it-ce ju-co-ce cols-12 bg-black-2">
-            <div class="bg-trans pad-20 bor-rad-5 dis-flex-col">
-                <div class="login-form">
-                    <form method="post" class="pos-abs dis-flex-col al-it-ce">
-                        <h2>Login</h2>
-                        <div class="input-box">
-                            <input type="email" name="email" required>
-                            <label for="email">Email</label>
-                            <i></i>
-                        </div>
-                        <div class="input-box">
-                            <input type="password" name="password" required>
-                            <label for="password">Password</label>
-                            <i></i>
-                        </div>
-                        <p>Don't have an Account ? <a href="<?= ROOT ?>/signup">Create an Account</a></p>
-                        <button type="submit">Login</button>
-                    </form>
-                </div>
+        <main class="login-container">
+            <div class="login left-section">
+                <form method="post" class="dis-flex-col al-it-ce">
+                    <h2>Login</h2>
+                    <div class="input-box">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" required>
+                        <i></i>
+                    </div>
+                    <div class="input-box">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" required>
+                        <i></i>
+                    </div>
+                    <p>Don't have an Account ? <br/> <a href="<?= ROOT ?>/signup">Create an Account</a></p>
+                    <button type="submit">Login</button>
+                </form>
             </div>
-        </div>
+
+            <div class="login right-section">
+                <img src="<?= ROOT ?>/assets/images/icons/login.jpg" alt="login">
+            </div>
+        </main>
+
     </div>
 </body>
 
