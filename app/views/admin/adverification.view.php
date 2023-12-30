b9f2d20e5a8f44e21643392af7a31ea70f3f3ae0
hansimadhusha <madhushahansini.perera@gmail.com>
2023-12-30 23:04:47 +0530

    single ad view

--- app/views/admin/adverification.view.php
+++ app/views/admin/adverification.view.php
@@ -12,7 +12,7 @@
         <section class="cols-10 dis-flex">
             <div class="mar-10 wid-100 dis-flex-col pad-20 gap-10 bor-rad-5" style="justify-content:stretch; align-items:stretch">
 
-                <div class="flex-1 dis-flex-col gap-10 mar-bot-10 mar-top-10">
+                <div class="flex-1 dis-flex-col gap-10 mar-bot-10 mar-top-10" style="max-height: 60vh; overflow:auto; padding-right: 10px">
 
                     <?php foreach($ads as $ad){
                         $this->view('admin/components/ad',(array)$ad);
