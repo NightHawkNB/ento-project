3adbb655caac392a9b53cf8fae6dd791e4abcfcb
NightHawkNB <nipunbathiya1256@outlook.com>
2024-01-04 21:49:29 +0530

    Signup view updates

--- app/views/includes/auth/signup_client.view.php
+++ app/views/includes/auth/signup_client.view.php
@@ -122,35 +122,6 @@
                             <i><?= (array_key_exists('confirmPass', $errors)) ? $errors['confirmPass'] : '' ?></i>
                         </div>
                     </div>
-                    <div class="input-box <?= (!empty($errors['user_type'])) ? 'error' : '' ?>">
-                        <select name="user_type" id="user_type" onchange="view_box()">
-                            <option value="" selected disabled>Account Type</option>
-                            <option <?= ($prev->user_type == 'client' ? 'selected' : '') ?> value="client">Client</option>
-                            <option <?= ($prev->user_type == 'singer' ? 'selected' : '') ?> value="singer">Singer</option>
-                            <option <?= ($prev->user_type == 'band' ? 'selected' : '') ?> value="band">Band</option>
-                            <option <?= ($prev->user_type == 'venuemanager' ? 'selected' : '') ?> value="venuemanager">Venue Manager</option>
-                        </select>
-                        <i></i>
-                    </div>
-
-                    <div id="singer-box" class="user_type_box input-box <?= (!empty($errors['rate'])) ? 'error' : '' ?>">
-                        <label for="rate">Rate</label>
-                        <input type="text" name="rate">
-                        <i></i>
-                    </div>
-
-                    <div id="band-box" class="user_type_box group">
-                        <div class="input-box <?= (!empty($errors['packages'])) ? 'error' : '' ?>">
-                            <label for="packages">Packages</label>
-                            <input type="text" name="packages">
-                            <i></i>
-                        </div>
-                        <div class="input-box <?= (!empty($errors['location'])) ? 'error' : '' ?>">
-                            <label for="location">Location</label>
-                            <input type="text" name="location">
-                            <i></i>
-                        </div>
-                    </div>
 
                     <div class="terms-div">
                         <input <?= set_value('terms') ? 'checked' : '' ?> type="checkbox" name="terms" id="terms" required>
@@ -164,7 +135,8 @@
                         <div class="text-center"></div>
                     <?php endif; ?>
                     <p class="">Already have an Account ? <a href="<?= ROOT ?>/login">Login</a></p>
-                    <button type="submit">Next</button>
+                    <button type="submit">Signup</button>
+                    <a href="<?= ROOT ?>/signup" class="blue-btn">Back</a>
                 </form>
             </div>
 
@@ -175,25 +147,7 @@
 
         <script>
             const user_type = document.getElementById('user_type')
-            const singer_box = document.getElementById('singer-box')
-            const band_box = document.getElementById('band-box')
             const side_image = document.getElementById('signup-right-image')
-
-            function view_box() {
-                let type = user_type.value
-
-                if(type === "singer") {
-                    if(!singer_box.classList.contains('selected')) singer_box.classList.add('selected')
-                    side_image.src = "<?= ROOT ?>/assets/images/icons/auth/singer.jpg"
-                } else if(type === "band") {
-                    if(!band_box.classList.contains('selected')) band_box.classList.add('selected')
-                    side_image.src = "<?= ROOT ?>/assets/images/icons/auth/band.jpg"
-                } else {
-                    if(singer_box.classList.contains('selected')) singer_box.classList.remove('selected')
-                    if(band_box.classList.contains('selected')) band_box.classList.remove('selected')
-                    side_image.src = "<?= ROOT ?>/assets/images/icons/auth/signup.jpg"
-                }
-            }
 
             // City data for each province
             const cityData = {
---
+++ app/views/includes/auth/user_type.view.php
@@ -0,0 +1,2 @@
+<a href="<?= ROOT ?>/signup/client"><button>Client</button></a>
+<a href="<?= ROOT ?>/signup/serviceprovider"><button>Service Provider</button></a>
