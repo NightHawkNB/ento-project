ec145ce700375174ea32b3dac5fcf6f4b86e1603
NightHawkNB <nipunbathiya1256@outlook.com>
2024-01-06 15:19:04 +0530

    Bug Fixes

--- app/views/includes/auth/user_type.view.php
+++ app/views/includes/auth/user_type.view.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="<?= ROOT ?>/assets/css/auth/user_selection.css">
 
-
 <h1>Step into the Spotlight: Choose Your Role !</h1>
 
 <div class="content">
@@ -10,7 +9,7 @@
         </div>
         <div class="back">
             <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+                <p> Consectetur adipisicing elit. Possimus, praesentium? </p>
                 <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
             </div>
         </div>
@@ -29,15 +28,15 @@
     </a>
 
     <a class="card" href="<?= ROOT ?>/signup/band"">
-        <div class="front" style="background-image: url(//source.unsplash.com/300x403)">
-            <p>Band</p>
+    <div class="front" style="background-image: url(//source.unsplash.com/300x403)">
+        <p>Band</p>
+    </div>
+    <div class="back">
+        <div>
+            <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+            <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
         </div>
-        <div class="back">
-            <div>
-                <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
-                <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
-            </div>
-        </div>
+    </div>
     </a>
 
     <a class="card" href="<?= ROOT ?>/signup/venuem">
