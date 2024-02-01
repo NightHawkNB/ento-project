db27fc7b70300f457fd117dfac769ed9b81b8828
hansimadhusha <madhushahansini.perera@gmail.com>
2024-02-01 18:25:13 +0530

    chart js

--- app/views/admin/useraccounts.view.php
+++ app/views/admin/useraccounts.view.php
@@ -1,5 +1,5 @@
 <html lang="en">
-<?php $this->view('includes/head') ?>
+<?php $this->view('includes/head',['style'=>'admin/usermanagement.css']) ?>
 <body>
     <div class="main-wrapper">
         <?php $this->view('includes/header') ?>
@@ -11,9 +11,19 @@
 
             <section class="cols-10 dis-flex">
                 <div class="glass-bg mar-10 wid-100  dis-flex-col pad-20 gap-10 bor-rad-5">
-                    <a href="profile/verify" class="push-right">
-                        <button class="btn-lay-2 hover-pointer"  style="background-color:blue; text-align:right; border: none" >Filter by</button>
-                    </a>
+                    <div class="wrapper">
+                        <div id="search-container">
+                            <input type="search" id="search-input" placeholder="search product name here..."/>
+                            <button id="search"git>Search</button>
+                        </div>
+                        <div id="buttons">
+                            <button class="button-value">All</button>
+                            <button class="button-value">Client</button>
+                            <button class="button-value">Admin</button>
+                            <button class="button-value">CCA</button>
+                            <button class="button-value">SP</button>
+                        </div>
+                    </div>
 
                     <div class="flex-1 dis-flex-col gap-10 mar-bot-10 mar-top-10" style="max-height: 60vh; overflow: auto; padding-right: 10px">
                         <?php
