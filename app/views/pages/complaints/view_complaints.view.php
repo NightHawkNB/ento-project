ed2f923cdee680bf4aa0ba5ce7c2348e8a2737cf
thisara-dil <thisarap8@gmail.com>
2024-02-01 14:20:42 +0530

    complaints transparent

--- app/views/pages/complaints/view_complaints.view.php
+++ app/views/pages/complaints/view_complaints.view.php
@@ -11,10 +11,7 @@
             </section>
         <?php endif; ?>
         <section class="cols-10 pad-20 mar-bot-10">
-            <div class="bg-white dis-flex-col gap-20 bg-lightgray flex-1 pad-20">
-                <div class="dis-flex ju-co-sb">
-                    <h1 class="txt-c-black">Complaints</h1>
-                </div>
+            <div class="bg-trans dis-flex-col gap-20 bg-lightgray flex-1 pad-20">
                 <div class="dis-flex-col bg-trans gap-10 pad-20 txt-ali-cen">
                     <?php
                     if(!empty($complaints)) {
