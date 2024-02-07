1af25c85b6131e2ebfaa8d25d39273ca3d934d7f
AkilaDemotte <akilamilindademotte@gmail.com>
2024-02-07 09:49:02 +0530

    Add filers for tickets

--- app/views/client/bought_tickets.view.php
+++ app/views/client/bought_tickets.view.php
@@ -1,4 +1,3 @@
-
 <html lang="en">
 <script src="<?= ROOT ?>/assets/scripts/qrcode.min.js"></script>
 <?php $this->view('includes/head') ?>
@@ -9,65 +8,65 @@
     <main class="dashboard-main">
         <section class="cols-2 sidebar">
             <?php $this->view('includes/sidebar') ?>
-            <section  class="wid-100 pad-10 dis-flex-col al-it-ce">
-            </section>
         </section>
-<!--toggle btn html-->
+        <section class="wid-100 pad-10 dis-flex-col al-it-ce">
+
+            <!--toggle btn html-->
             <div>
-                <div class="button-box-res dis-flex" >
+                <div class="button-box-res dis-flex">
                     <div class="btn-res"></div>
-                    <button  type="button" class="toggle-btn-res "  onclick="hideShowCurrent()">
+                    <button type="button" class="toggle-btn-res " onclick="hideShowCurrent()">
                         Current
                     </button>
-                    <button  type="button" class="toggle-btn-res"  onclick="hideShowOutdated()">
+                    <button type="button" class="toggle-btn-res" onclick="hideShowOutdated()">
                         Outdated
                     </button>
                 </div>
 
             </div>
-<!--        current tickets-->
-        <section id="current" class="wid-100 pad-10 dis-flex-col al-it-ce  hei-100">
+            <!--        current tickets-->
+            <section id="current" class="wid-100 pad-10 dis-flex-col al-it-ce  hei-100">
 
-            <h1 class="mar-10-0 txt-c-white txt-w-bold" style="font-size: 1.5rem"> Current Bought Tickets</h1>
+                <h1 class="mar-10-0 txt-c-white txt-w-bold" style="font-size: 1.5rem"> Current Bought Tickets</h1>
 
-            <div class="wid-80 glass-bg dis-flex gap-20 pad-20 hei-100 wid-100">
+                <div class="wid-80 glass-bg dis-flex gap-20 pad-20 hei-100 wid-100">
 
-                <?php
-                $currentDateTime = date('HY-m-d H:i:s');
-                if(!empty($bought_tickets)) {
-                    foreach($bought_tickets as $bought_ticket) {
-                        if($currentDateTime > $bought_ticket->start_time){
-                            $this->view('client/components/bought_ticket_current', (array)$bought_ticket);
+                    <?php
+                    $currentDateTime = date('HY-m-d H:i:s');
+                    if (!empty($bought_tickets)) {
+                        foreach ($bought_tickets as $bought_ticket) {
+                            if ($currentDateTime > $bought_ticket->start_time) {
+                                $this->view('client/components/bought_ticket_current', (array)$bought_ticket);
+                            }
                         }
+                    } else {
+                        echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No bought tickets to show</h3>";
                     }
-                } else {
-                    echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No bought tickets to show</h3>";
-                }
-                ?>
-            </div>
+                    ?>
+                </div>
 
-        </section>
-<!--        outdated tickets-->
-        <section id="outdated" class="wid-100 pad-10 dis-flex-col al-it-ce  hei-100">
+            </section>
+            <!--        outdated tickets-->
+            <section id="outdated" class="wid-100 pad-10 dis-flex-col al-it-ce  hei-100">
 
-            <h1 class="mar-10-0 txt-c-white txt-w-bold" style="font-size: 1.5rem"> Outdated Bought Tickets</h1>
+                <h1 class="mar-10-0 txt-c-white txt-w-bold" style="font-size: 1.5rem"> Outdated Bought Tickets</h1>
 
-            <div class="wid-80 glass-bg dis-flex gap-20 pad-20 hei-100 wid-100">
+                <div class="wid-80 glass-bg dis-flex gap-20 pad-20 hei-100 wid-100">
 
-                <?php
-                $currentDateTime = date('HY-m-d H:i:s');
-                if(!empty($bought_tickets)) {
-                    foreach($bought_tickets as $bought_ticket) {
-                        if($currentDateTime < $bought_ticket->start_time){
-                            $this->view('client/components/bought_ticket_outdated', (array)$bought_ticket);
+                    <?php
+                    $currentDateTime = date('HY-m-d H:i:s');
+                    if (!empty($bought_tickets)) {
+                        foreach ($bought_tickets as $bought_ticket) {
+                            if ($currentDateTime < $bought_ticket->start_time) {
+                                $this->view('client/components/bought_ticket_outdated', (array)$bought_ticket);
+                            }
                         }
+                    } else {
+                        echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No bought tickets to show</h3>";
                     }
-                } else {
-                    echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No bought tickets to show</h3>";
-                }
-                ?>
-            </div>
-
+                    ?>
+                </div>
+            </section>
     </main>
 </div>
 </body>
@@ -79,62 +78,58 @@
     var outdated = document.getElementById('outdated');
     var buttons = document.querySelector('.btn-res');
 
-    function hideShowCurrent()
-    {
+    function hideShowCurrent() {
         var isHiddenCurrent = current.classList.contains('hide');
         var isHiddenOutdated = outdated.classList.contains('hide');
 
 
-        if(!isHiddenOutdated)
-        {
+        if (!isHiddenOutdated) {
             outdated.classList.add('hide');
             leftClick();
         }
 
-        if(isHiddenCurrent)
-        {
+        if (isHiddenCurrent) {
             current.classList.remove('hide');
             leftClick();
         }
     }
-    function hideShowOutdated()
-    {
-        var isHiddenCurrent = current.classList.contains('hide');
-        var isHiddenOutdated = outdated.classList.contains('hide');
+
+    function hideShowOutdated() {
+        let isHiddenCurrent = current.classList.contains('hide');
+        let isHiddenOutdated = outdated.classList.contains('hide');
 
 
-        if(!isHiddenCurrent)
-        {
+        if (!isHiddenCurrent) {
             current.classList.add('hide');
             rightClick();
         }
 
-        if(isHiddenOutdated)
-        {
+        if (isHiddenOutdated) {
             outdated.classList.remove('hide');
             rightClick();
         }
     }
 
     // toggle button
-    function leftClick(){
+    function leftClick() {
         buttons.style.left = '0'
     }
 
-    function rightClick(){
+    function rightClick() {
         buttons.style.left = '110px'
     }
 
     // show current data when refresh and stay on outdated data when rating is added.
 
-    var currentTab="<?=$currentTab?>"
+    let currentTab = "<?=$currentTab?>"
 
-    document.addEventListener('DOMContentLoaded',function (){
-        if(currentTab == 'current'){
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log(currentTab)
+        if (currentTab === 'current') {
             outdated.classList.add('hide');
             current.classList.remove('hide');
             leftClick();
-        }else{
+        } else {
             current.classList.add('hide');
             outdated.classList.remove('hide');
             rightClick();
