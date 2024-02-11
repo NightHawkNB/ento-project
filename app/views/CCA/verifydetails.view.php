05cccd168527f7acf786afe293cd74c7f9f9758f
thisara-dil <thisarap8@gmail.com>
2024-02-11 19:13:57 +0530

    verify page update

--- app/views/CCA/verifydetails.view.php
+++ app/views/CCA/verifydetails.view.php
@@ -1,139 +1,131 @@
 <html lang="en">
 <?php $this->view('includes/head') ?>
 <style>
-    .verify-container{
-   /* position: relative;
-    max-width: 900px;*/
-    width: 100%;
-    background: #fff;
-    padding: 25px;
-    border-radius: 8px;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-}
+    .verify-container {
+        width: 100%;
+        height: 100%;
+        background: #fff;
+        padding: 25px;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        justify-content: stretch;
 
-.verify-container header{
-    font-size: 1.5rem;
-    color: #333;
-    font-weight: 500;
-    text-align: center;
-}
-.verify-container .form{
-    position: relative;
-    /* border-style :solid;
-    border-width: 5px; */
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    width: 125%;
-    display: flex;
-    
-  
-}
+        .content {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+        }
+    }
 
+    .verify-container header {
+        font-size: 1.5rem;
+        color: #333;
+        font-weight: 500;
+        text-align: center;
+    }
 
-.verify-container .form{
-    margin-top: 30px;
-}
+    .form .input-box {
+        width: 100%;
+    }
 
-.form .input-box{
-    width: 100%;
-    margin-top: 20px;
-}
-.input-box label{
-    color: #333;
-}
+    .input-box label {
+        color: #333;
+    }
 
-.form .input-box input{
-    position: relative;
-    height: 50px;
-    width: 100%;
-    outline: none;
-    font-size: 1rem;
-    color: #707070;
-    margin-top: 8px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    padding: 0 15px;
-}
+    .form .input-box input {
+        height: 50px;
+        width: 100%;
+        outline: none;
+        font-size: 1rem;
+        color: #707070;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding: 0 15px;
+    }
 
-.form .colum{
-    display: flex;
-    column-gap: 10px;
-}
+    .form .colum {
+        display: flex;
+        column-gap: 10px;
+    }
 
-.btn{ 
-    justify-content: center;
-    border: none;
-    outline: none;
-    border-radius: 8px;
-    margin: 10px;
-    padding: 20px;
-    color: #333;
-}
+    .btn {
+        justify-content: center;
+        border: none;
+        outline: none;
+        border-radius: 8px;
+        padding: 20px;
+        color: #333;
+    }
 
-.verify-container .right{
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    margin: 10px;
-}
+    .verify-container .right {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
 
-.verify-container .left{
-    width: 30%;
-    display: flex;
-    flex-direction: column;
-    margin: 10px;
-}
+        .photo {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+        }
+    }
+
+    .verify-container .left {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
 </style>
 <body>
-    <div class="main-wrapper">
-        <?php $this->view('includes/header') ?>
+<div class="main-wrapper">
+    <?php $this->view('includes/header') ?>
 
-        <main class="dashboard-main">
-            <section class="cols-2 sidebar">
-                <?php $this->view('includes/sidebar') ?>
-            </section>
-            <section class="cols-10">
+    <main class="dashboard-main">
+        <section class="cols-2 sidebar">
+            <?php $this->view('includes/sidebar') ?>
+        </section>
+        <section class="cols-10 pad-20 dis-flex wid-100 hei-100 ju-co-ce al-it-ce">
             <div class="verify-container">
-        <header>Profile</header>
-        <form action="#" class="form">
-            <div class="content">
-                <div class="left">
-                    <div class="input-box">
-                        <label>Full Name</label>
-                        <input type="text" placeholder="Enter your full name"/>
-                    </div>
-                    <div class="input-box">
-                        <label>Address</label>
-                        <input type="text" placeholder="Enter your address"/>
-                    </div>
-                    <div class="colum">
-                        <div class="input-box">
-                            <label>Age</label>
-                            <input type="text" placeholder="Enter your age"/>
+                <header>Profile</header>
+                <form action="#" class="form" style="width: 100%">
+                    <div class="content">
+                        <div class="left">
+                            <div class="input-box">
+                                <label>Full Name</label>
+                                <input type="text" placeholder="Enter your full name"/>
+                            </div>
+                            <div class="input-box">
+                                <label>Address</label>
+                                <input type="text" placeholder="Enter your address"/>
+                            </div>
+                            <div class="colum">
+                                <div class="input-box">
+                                    <label>Age</label>
+                                    <input type="text" placeholder="Enter your age"/>
+                                </div>
+                                <div class="input-box">
+                                    <label>Gender</label>
+                                    <input type="text" placeholder="Enter your gender"/>
+                                </div>
+                            </div>
                         </div>
-                        <div class="input-box">
-                            <label>Gender</label>
-                            <input type="text" placeholder="Enter your gender"/>
+                        <div class="right">
+                            <div class="photo">
+                                <img src="<?= ROOT ?>/assets/images/vrequests/1.jpg" style="width: 200px" alt="id front">
+                                <br>
+                                <img src="<?= ROOT ?>/assets/images/vrequests/1.jpg" style="width: 200px" alt="id back">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="right">
-                    <div class="photo">
-                        <img src="/Users/sadeeisback/Desktop/idf.png" alt="id front" >
-                        <br>
-                        <img src="/Users/sadeeisback/Desktop/idb.png" alt="id back" >
+                    <div class="buttons">
+                        <button type="submit" class="btn">Verify</button>
+                        <button type="submit" class="btn">Declined</button>
                     </div>
-                </div>
+                </form>
             </div>
-            <div class="buttons">
-                <button type="submit" class="btn">Verify</button>
-                <button type="submit" class="btn">Declined</button>
-            </div>
-        </form>
-    </div>
-                </section>
-        </main>
-    </div>
+        </section>
+    </main>
+</div>
 </body>
 </html>
