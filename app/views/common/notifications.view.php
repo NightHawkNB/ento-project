252818f4c2f2b6e77f2f5bb1c8fa63bf8bd6c4a5
AkilaDemotte <akilamilindademotte@gmail.com>
2024-04-21 08:54:30 +0530

    some changes in notifications

--- app/views/common/notifications.view.php
+++ app/views/common/notifications.view.php
@@ -11,17 +11,45 @@
         <section class="wid-100 pad-10 dis-flex-col al-it-ce">
 
             <h1 class="mar-10-0 txt-c-black txt-b-bold" style="font-size: 1.5rem"> Notifications</h1>
-            <div class="txt-ali-lef bg-black-1 txt-c-white wid-100 mar-10 pad-10 bor-rad-5 glass-bg">
+            <div class="txt-ali-lef bg-white-90 txt-c-black wid-100 mar-10 pad-10 bor-rad-5 glass-bg">
+                <h3>New</h3>
+                <br>
+                <div class="new">
+
                     <?php
                     if(!empty($all_notifications)) {
                         foreach($all_notifications as $notification) {
+                            if($notification->viewed == 0){
                                 $this->view('common/components/notification', (array)$notification);
 //                                show($notification);
+                            }
                         }
                     } else {
                         echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No notifications to show</h3>";
                     }
                     ?>
+
+                </div>
+                <br>
+                <hr>
+                <br>
+                <h3>Viewed</h3>
+                <br>
+                <div class="viewed">
+
+                    <?php
+                    if(!empty($all_notifications)) {
+                        foreach($all_notifications as $notification) {
+                            if($notification->viewed == 1){
+                                $this->view('common/components/notification', (array)$notification);
+                            }
+                        }
+                    } else {
+                        echo "<h3 class='txt-c-white wid-100 dis-flex ju-co-ce'>No notifications to show</h3>";
+                    }
+                    ?>
+
+                </div>
             </div>
 
         </section>
