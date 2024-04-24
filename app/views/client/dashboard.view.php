96e4783863a53c71c63bb17544bd4b605ca71cf1
AkilaDemotte <akilamilindademotte@gmail.com>
2024-04-24 13:30:17 +0530

    updates

--- app/views/client/dashboard.view.php
+++ app/views/client/dashboard.view.php
@@ -9,21 +9,7 @@
             <?php $this->view('includes/sidebar') ?>
         </section>
         <section class="pad-10 dis-flex-col al-it-ce wid-100">
-            <h1 class="mar-10-0 txt-c-black txt-w-bold" style="font-size: 1.5rem">Dashboard</h1>
-
-<!--            <div class="slider-frame">-->
-<!--                <div class="slide-images">-->
-<!--                    <div class="img-container">-->
-<!--                        <img src="--><?php //=ROOT?><!--/assets/images/event/EVENT_11812_1708704770.png">-->
-<!--                    </div>-->
-<!--                    <div class="img-container">-->
-<!--                        <img src="--><?php //=ROOT?><!--/assets/images/event/EVENT_13920_1710128400.png">-->
-<!--                    </div>-->
-<!--                    <div class="img-container">-->
-<!--                        <img src="--><?php //=ROOT?><!--/assets/images/event/EVENT_11812_1708704770.png">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
+<!--            <h1 class="mar-10-0 txt-c-black txt-w-bold" style="font-size: 1.5rem">Dashboard</h1>-->
             <div id="slider">
                 <figure>
                     <img src="<?=ROOT?>/assets/images/event/EVENT_11812_1708704770.png">
