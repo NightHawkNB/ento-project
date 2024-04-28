c13e19c879b563b10f642ac1892469622d89c881
NightHawkNB <nipunbathiya1256@outlook.com>
2024-04-28 11:40:44 +0530

    Reservation details - UI changes

--- app/views/common/events/components/singer.view.php
+++ app/views/common/events/components/singer.view.php
@@ -2,7 +2,7 @@
     <div class="venue-item">
         <img src="<?= ROOT.$image ?>" alt="singer-image" style="width: 50px; aspect-ratio: 1/1; border-radius: 50%">
         <p><?= $title ?></p>
-        <p><?= $details ?></p>
+        <p class="sp-details"><?= $details ?></p>
     </div>
     <input type="checkbox" name="SING_<?= $ad_id ?>" id="SING_<?= $ad_id ?>" value="<?= $ad_id ?>" style="display: none">
 </label>
